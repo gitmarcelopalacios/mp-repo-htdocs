543f7e30f2fe62b1588eda32d8a2ccf10da38f48
Marcelo <correodemarcelopalacios@gmail.com>
2020-08-11 18:39:12 -0300

    Actualización de JS

---
+++ 01-Ejemplos 2020-08/05-PHP/06-echo and print/040-display variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . " with point<br>";
+echo "Study PHP at " , $txt2 , " with comma<br>";
+echo $x + $y;
+?>
+
+</body>
+</html>
