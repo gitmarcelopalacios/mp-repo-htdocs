be80a3f8236b18c2cd97dbad26d9ad0ab1cc9c29
Marcelo <correodemarcelopalacios@gmail.com>
2020-08-13 23:07:26 -0300

    JS

--- 01-Ejemplos 2020-08/05-PHP/07-data types/053b-object.php
+++ 01-Ejemplos 2020-08/05-PHP/07-data types/053b-object.php
@@ -3,16 +3,20 @@
 <body>
 
 <?php
+
+
 class Car {
   function Car() {
     $this->model = "VW";
   }
 }
+
 // create an object
 $herbie = new Car();
 
 // show object properties
 echo $herbie->model;
+
 ?>
 
 </body>
---
+++ 01-Ejemplos 2020-08/07-Python/2020-08-05 Sentencias Repasadas.py
@@ -0,0 +1,58 @@
+# input
+
+texto=input("ingrese texto: ")
+
+
+
+# print
+
+print("Hello, World!")
+
+# indentacion
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+# comentarios
+
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment.
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+# variables
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4 # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = "John"
+# is the same as
+x = 'John'
+
+#Legal variable names:
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x = 5
+print(type(x))
+
